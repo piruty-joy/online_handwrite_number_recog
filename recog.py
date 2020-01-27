01959f3ef01ca5a2ce5e13de53db0ac44b2883bf
piruty <8793641+piruty@users.noreply.github.com>
2020-01-28 08:41:51 +0900

    Pipenv管理に移行 & ライブラリをkerasベースに変更

--- recog.py
+++ recog.py
@@ -1,25 +1,14 @@
 #coding: utf-8
 import numpy as np
-import chainer
-import chainer.functions as F
+import tensorflow
 from PIL import Image
 
 def recognise(img):
 
     img = img.convert("L").resize((28,28))
     X = np.array(img).astype(np.float32)
-    X = X.reshape((1,1,28,28))
+    X = X.reshape((1,28,28,1))
 
-    import pickle
-    model = pickle.load(open("model.pkl", "rb"))
-
-    def forward(x_data):
-        x = chainer.Variable(x_data)
-        h = F.max_pooling_2d(F.relu(model.conv1(x)), 2)
-        h = F.max_pooling_2d(F.relu(model.conv2(h)), 2)
-        h = F.dropout(F.relu(model.l1(h)), train=False)
-        y = model.l2(h)
-        return y.data
-
-    result = forward(X)[0]
+    model = tensorflow.keras.models.load_model('model.h5')
+    result = model.predict(X)
     return int(np.argmax(result))
